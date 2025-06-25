878f9c6cdfb3648f62ce91881d62a73bec91e28d
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2025-06-24 23:15:07 -0700

    Time: 133 ms (49.43%), Space: 19.3 MB (27.01%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,17 +1,26 @@
+"""
+    max heap
+    - calculate freq count
+    - store it in a max heap as v,k
+    - pop each element from heap and store in tmp
+    - push the tmp again to heap if tmp not empty
+"""
+
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = Counter(tasks)
         hq = []
         for k,v in count.items():
             heapq.heappush(hq, (-v,k))
+        
         time = 0
         while hq:
-            tmp  = []
+            tmp = []
             for i in range(n+1):
                 if hq:
                     freq, task = heapq.heappop(hq)
                     freq += 1
-                    if freq != 0:
+                    if freq:
                         tmp.append((freq, task))
                 time += 1
 
@@ -20,4 +29,3 @@ class Solution:
             for item in tmp:
                 heapq.heappush(hq, item)
         return time
-        
