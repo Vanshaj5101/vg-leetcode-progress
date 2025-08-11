300b601efb5dcb370392e489d7abd873fd35060c
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2025-08-10 18:54:22 -0700

    Time: 0 ms (100%), Space: 17.9 MB (38.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,25 +3,16 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        if n==2:
+        elif n == 2:
             return max(nums[0], nums[1])
-        
-        # n[0:n-2]
-        prev2 = nums[0]
-        prev1 = max(nums[0], nums[1])
-        for i in range(2, n-1):
-            curr = max(prev2 + nums[i], prev1)
-            prev2 = prev1
-            prev1 = curr
-        
-        result1 = prev1
-
-        prev2 = nums[1]
-        prev1 = max(nums[1], nums[2])
-        for i in range(3, n):
-            curr = max(prev2 + nums[i], prev1)
-            prev2 = prev1
-            prev1 = curr
-        result2 = prev1
-
-        return max(result1, result2)
+        else:
+            def min_cost(costs):
+                l = len(costs)
+                dp = [0] * l
+                dp[0] = costs[0]
+                dp[1] = max(costs[0], costs[1])
+                for i in range(2, l):
+                    dp[i] = max(dp[i-2] + costs[i], dp[i-1])
+                return dp[l-1]
+            
+            return max(min_cost(nums[0:n-1]), min_cost(nums[1:n]))
