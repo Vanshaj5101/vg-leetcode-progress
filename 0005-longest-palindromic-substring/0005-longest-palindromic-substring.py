64fe0c86b7a1beb83f91e0896c873994e5eb3ecb
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2025-08-11 23:48:23 -0700

    Time: 258 ms (78.63%), Space: 17.9 MB (59.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,21 +1,43 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # maxlen = 0
+        # sp = 0
+        # n = len(s)
+        # dp = [[-1 for i in range(n)] for j in range(n)]
+        
+        # def ispalindrome(s, i, j):
+        #     if i >= j:
+        #         return 1
+        #     elif dp[i][j] != -1:
+        #         return dp[i][j]
+        #     elif s[i] == s[j]:
+        #         dp[i][j] = ispalindrome(s, i+1, j-1)
+        #         return dp[i][j]
+        #     else:
+        #         return 0
+        
+        # for i in range(n):
+        #     for j in range(n):
+        #         if ispalindrome(s, i, j):
+        #             if j - i + 1 > maxlen:
+        #                 maxlen = j - i + 1
+        #                 sp = i
+        # return s[sp: sp + maxlen]
         
         def expand(i, j):
             while i >= 0 and j < len(s) and s[i] == s[j]:
                 i -= 1
                 j += 1
-            return s[i+1:j]
+            return s[i+1: j]
         
         longest = ""
 
         for i in range(len(s)):
             odd = expand(i, i)
-            even = expand(i, i+1)
+            even = expand(i, i + 1)
 
             if len(odd) > len(longest):
                 longest = odd
             if len(even) > len(longest):
                 longest = even
-        
         return longest
