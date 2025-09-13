a59a476918ea6b97f3a397917dfaaf3be4a6200c
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2025-09-12 17:47:45 -0700

    Time: 18 ms (38.78%), Space: 22.6 MB (25.01%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,10 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hsh = defaultdict(list)
-        
+        hshmap = defaultdict(list)
         for s in strs:
-            char_set = [0] * 26
+            arr = [0] * 26
             for c in s:
-                char_set[ord(c) - ord('a')] += 1
-            hsh[tuple(char_set)].append(s)
-        return list(hsh.values())
+                arr[ord(c) - ord('a')] += 1
+            hshmap[tuple(arr)].append(s)
+        
+        return list(hshmap.values())
