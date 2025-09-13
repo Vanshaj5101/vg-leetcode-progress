1eca67978e562429de0edafec766761b5e3ed955
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2025-09-12 18:06:41 -0700

    Time: 1 ms (87.98%), Space: 17.8 MB (69.54%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,12 +4,13 @@ class Solution:
         cols = defaultdict(set)
         squares = defaultdict(set)
 
-        for r in range(9):
-            for c in range(9):
-                if board[r][c]!='.':
-                    if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]:
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    if board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in squares[(i//3,j//3)]:
                         return False
-                    rows[r].add(board[r][c])
-                    cols[c].add(board[r][c])
-                    squares[(r//3, c//3)].add(board[r][c])
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    squares[(i//3,j//3)].add(board[i][j])
+        
         return True
