15179d6122b461e61b6a98383eb833e08ab91958
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2025-11-12 22:54:11 -0700

    Time: 96 ms (46.28%), Space: 51.5 MB (21.86%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,21 +1,17 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         n = len(intervals)
-        if n == 1:
-            return 0
-        
         intervals.sort(key=lambda x:x[0])
+        remove = 0
         prev = intervals[0][1]
         i = 1
-        remove = 0
+        
         while i < n:
             if intervals[i][0] >= prev:
                 prev = intervals[i][1]
             else:
-                prev = min(intervals[i][1], prev)
                 remove += 1
+                prev = min(intervals[i][1], prev)
             i += 1
         
         return remove
-        
-
