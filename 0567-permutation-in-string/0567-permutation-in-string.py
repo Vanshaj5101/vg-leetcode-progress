990e352e9569dfbd5e51055a40a8e0802a23d70c
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2025-12-25 12:28:51 -0700

    Time: 19 ms (65.13%), Space: 17.4 MB (95.72%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,15 +1,41 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        # sliding window
+        # s1_len = len(s1)
+        # s2_len = len(s2)
+        # if s1_len > s2_len:
+        #     return False
+
+        # s1 = ''.join(sorted(s1))
+
+        # for i in range(s2_len-s1_len+1):
+        #     substring = s2[i:s1_len + i]
+        #     substring = ''.join(sorted(substring))
+        #     if s1 == substring:
+        #         return True
+        # return False
+
+
+        # sliding window approach 2
+
         s1_len = len(s1)
         s2_len = len(s2)
         if s1_len > s2_len:
             return False
+        
+        s1_freq = [0] * 26
+        s2_freq = [0] * 26
 
-        s1 = ''.join(sorted(s1))
-
-        for i in range(s2_len-s1_len+1):
-            substring = s2[i:s1_len + i]
-            substring = ''.join(sorted(substring))
-            if s1 == substring:
+        for i in range(s1_len):
+            s1_freq[ord(s1[i]) - ord('a')] += 1
+        
+        i = j = 0
+        while j < s2_len:
+            s2_freq[ord(s2[j]) - ord('a')] += 1
+            if j-i+1>s1_len:
+                s2_freq[ord(s2[i]) - ord('a')] -= 1
+                i += 1
+            if s1_freq == s2_freq:
                 return True
+            j += 1
         return False
