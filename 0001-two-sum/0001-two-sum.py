35a04936a18de43fddd301f4588e4f010a359ab6
Vanshaj Gupta <68272691+Vanshaj5101@users.noreply.github.com>
2026-09-05 17:14:02 -0700

    Time: 3 ms (53.6%), Space: 20.4 MB (41.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hshmap = dict()
+        res = []
         for i in range(len(nums)):
-            if target - nums[i] in hshmap:
-                return [i, hshmap[target - nums[i]]]
+            val = target - nums[i]
+            if val in hshmap.keys():
+                return [hshmap[val], i]
             hshmap[nums[i]] = i
         
+        return res
+
+        # TC : O(n)
+        # SC : O(n)
